548037d22dc4d22cea95d98e910a69b408c8a41c
Martin Zarutski <martinzar5@gmail.com>
2023-06-02 12:15:02 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿class HomeWork_task1
+{
+    static public void Main()
+    {
+        for (int i = 10; i < 100; i++)
+        {
+            if (i % 5 == 0)
+
+                Console.Write($" " + i);
+        }
+    }
+}
